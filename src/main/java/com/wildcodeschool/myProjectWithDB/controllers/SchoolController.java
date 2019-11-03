5bd146026e8334bcd7b3f81b9fce9fadbd5c5ff8
52-28 <aymeric.feillault@gmail.com>
2019-11-03 17:11:00 +0100

    put challenge

--- src/main/java/com/wildcodeschool/myProjectWithDB/controllers/SchoolController.java
+++ src/main/java/com/wildcodeschool/myProjectWithDB/controllers/SchoolController.java
@@ -3,7 +3,9 @@ package com.wildcodeschool.myProjectWithDB.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -81,5 +83,20 @@ public class SchoolController {
     		    idGeneratedByInsertion
     		);
     }
+    @PutMapping("/api/schools/{id}")
+    public School update(
+    	    @PathVariable int id,
+    	    @RequestParam(required = false) String name,
+    	    @RequestParam(required = false) Integer capacity,
+    	    @RequestParam(required = false) String country
+    	) {
+        School school = SchoolRepository.selectById(id);
+        SchoolRepository.update(
+            id,
+            name != null ? name : school.getName(),
+            capacity != null ? capacity : school.getCapacity(),
+            country != null ? country : school.getCountry()
+        );
+        return SchoolRepository.selectById(id);    }
 
 }
